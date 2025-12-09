512289048b774cac2ab9fb2ceb39a4abd2a019d0
N3RLUK <vovabelelia@gmail.com>
2025-12-09 08:28:06 +0200

    practical task 6-8

---
+++ practical_task_8/myblog/blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_details, name='post_details'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+]
